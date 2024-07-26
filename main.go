8340375f03deca04d9ed383aea4cc0f768226c05
cantti <cantti@outlook.com>
2024-07-26 15:06:15 +0200

    add path input field

--- main.go
+++ main.go
@@ -8,20 +8,20 @@ import (
 
 type PanelView struct {
 	dirPath string
-	dir     *tview.List
+	ui_list *tview.List
 }
 
 type Tui struct {
-	app         *tview.Application
-	pages       *tview.Pages
-	leftPanel   *PanelView
-	rightPanel  *PanelView
-	renameModal *tview.Modal
+	app            *tview.Application
+	ui_mainGrid    *tview.Grid
+	ui_pages       *tview.Pages
+	ui_renameModal *tview.Modal
+	leftPanel      *PanelView
+	rightPanel     *PanelView
 }
 
 func main() {
-	test := 123
-	tui := Tui{
+	tui := &Tui{
 		leftPanel:  &PanelView{},
 		rightPanel: &PanelView{}}
 
@@ -31,8 +31,7 @@ func main() {
 	user, _ := user.Current()
 	tui.leftPanel.dirPath = user.HomeDir
 	tui.rightPanel.dirPath = user.HomeDir
-	test = test + 1
-	tui.render()
+	draw(tui)
 
 	tui.updateDir(0)
 	tui.updateDir(1)
@@ -40,22 +39,19 @@ func main() {
 	tui.app.Run()
 }
 
-func (tui *Tui) render() {
-	tui.pages = tview.NewPages()
-	tui.app.SetRoot(tui.pages, true)
+func draw(tui *Tui) {
+	tui.ui_pages = tview.NewPages()
+	tui.app.SetRoot(tui.ui_pages, true)
 
-	mainGrid := tview.NewGrid().
-		SetRows(0, 1).
+	tui.ui_mainGrid = tview.NewGrid().
+		SetRows(1, 0, 1).
 		SetBorders(true)
 
-	tui.leftPanel.dir = tui.renderDir()
-	tui.rightPanel.dir = tui.renderDir()
+	drawDir(tui, tui.leftPanel, 0)
+	drawDir(tui, tui.rightPanel, 1)
 
-	mainGrid.AddItem(tui.leftPanel.dir, 0, 0, 1, 1, 0, 0, false)
-	mainGrid.AddItem(tui.rightPanel.dir, 0, 1, 1, 1, 0, 0, false)
-
-	bottomToolbar := tui.renderBottomToolbar()
-	mainGrid.AddItem(bottomToolbar, 1, 0, 1, 2, 0, 0, false)
+	bottomToolbar := tui.drawBottomToolbar()
+	tui.ui_mainGrid.AddItem(bottomToolbar, 2, 0, 1, 2, 0, 0, false)
 
 	modal := tview.NewModal().
 		SetText("Do you want to quit the application?").
@@ -64,56 +60,54 @@ func (tui *Tui) render() {
 			if buttonLabel == "Quit" {
 				tui.app.Stop()
 			} else {
-				tui.pages.HidePage("modal")
+				tui.ui_pages.HidePage("modal")
 			}
 		})
 
-	tui.renameModal = tview.NewModal().
+	tui.ui_renameModal = tview.NewModal().
 		SetText("hello")
 
-	tui.pages.AddPage("main", mainGrid, true, true)
-	tui.pages.AddPage("modal", modal, true, false)
-	tui.pages.AddPage("rename", tui.renameModal, true, false)
+	tui.ui_pages.AddPage("main", tui.ui_mainGrid, true, true)
+	tui.ui_pages.AddPage("modal", modal, true, false)
+	tui.ui_pages.AddPage("rename", tui.ui_renameModal, true, false)
 
-	tui.app.SetFocus(tui.leftPanel.dir)
+	tui.app.SetFocus(tui.leftPanel.ui_list)
 }
 
-func (tui *Tui) renderDir() *tview.List {
-	list := tview.NewList()
-	list.ShowSecondaryText(false)
-	return list
+func drawDir(tui *Tui, panel *PanelView, col int) {
+	inputField := tview.NewInputField().SetText(panel.dirPath)
+	tui.ui_mainGrid.AddItem(inputField, 0, col, 1, 1, 0, 0, false)
+	panel.ui_list = tview.
+		NewList().
+		ShowSecondaryText(false)
+	tui.ui_mainGrid.AddItem(panel.ui_list, 1, col, 1, 1, 0, 0, false)
 }
 
-func (tui *Tui) renderBottomToolbar() *tview.Grid {
+func (tui *Tui) drawBottomToolbar() *tview.Grid {
 	buttonGrid := tview.NewGrid().SetColumns(0, 0, 0, 0).SetGap(0, 1)
 	buttonGrid.AddItem(tview.NewButton("copy"), 0, 0, 1, 1, 0, 0, false)
 	buttonGrid.AddItem(tview.NewButton("move"), 0, 1, 1, 1, 0, 0, false)
 	buttonGrid.AddItem(tview.NewButton("rename").
 		SetSelectedFunc(tui.showRenameWin), 0, 2, 1, 1, 0, 0, false)
 	buttonGrid.AddItem(tview.NewButton("quit").
-		SetSelectedFunc(func() { tui.pages.ShowPage("modal") }), 0, 3, 1, 1, 0, 0, false)
+		SetSelectedFunc(func() { tui.ui_pages.ShowPage("modal") }), 0, 3, 1, 1, 0, 0, false)
 	return buttonGrid
 }
 
 func (tui *Tui) showRenameWin() {
-	i := tui.leftPanel.dir.GetCurrentItem()
-	text, _ := tui.leftPanel.dir.GetItemText(i)
-	tui.renameModal.SetText(text)
-	tui.pages.ShowPage("rename")
+	i := tui.leftPanel.ui_list.GetCurrentItem()
+	text, _ := tui.leftPanel.ui_list.GetItemText(i)
+	tui.ui_renameModal.SetText(text)
+	tui.ui_pages.ShowPage("rename")
 }
 
 func (tui *Tui) updateDir(dir int) {
-	var list *tview.List
-	var path string
-	if dir == 0 {
-		list = tui.leftPanel.dir
-		path = tui.leftPanel.dirPath
-	} else {
-		list = tui.rightPanel.dir
-		path = tui.rightPanel.dirPath
+	panel := tui.leftPanel
+	if dir == 1 {
+		panel = tui.rightPanel
 	}
-	files, _ := os.ReadDir(path)
+	files, _ := os.ReadDir(panel.dirPath)
 	for _, e := range files {
-		list.AddItem(e.Name(), "", 0, nil)
+		panel.ui_list.AddItem(e.Name(), "", 0, nil)
 	}
 }
