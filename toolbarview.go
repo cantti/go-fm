93b5356952f1f5a5b8fdc7f56d1f522c539a37db
cantti <cantti@outlook.com>
2024-08-02 01:03:15 +0200

    better rename button

--- toolbarview.go
+++ toolbarview.go
@@ -20,19 +20,21 @@ func newToolbarView(m *mainView) *toolbarView {
 	flex.AddItem(tview.NewBox(), 1, 0, false)
 
 	btnCopy := tview.NewButton(t.fmtBtn("F5", "Copy"))
+	flex.AddItem(btnCopy, 0, 1, false)
+	flex.AddItem(tview.NewBox(), 1, 0, false)
 
 	btnMove := tview.NewButton(t.fmtBtn("F6", "Move"))
+	flex.AddItem(btnMove, 0, 1, false)
+	flex.AddItem(tview.NewBox(), 1, 0, false)
 
-	btnRename := tview.NewButton("Rename")
+	btnRename := tview.NewButton(t.fmtBtn("Shift-F6", "Rename"))
 	btnRename.SetSelectedFunc(m.showRenameWin)
+	flex.AddItem(btnRename, 0, 1, false)
+	flex.AddItem(tview.NewBox(), 1, 0, false)
 
 	btnQuit := tview.NewButton(t.fmtBtn("F10", "Quit"))
 	btnQuit.SetSelectedFunc(func() { m.pages.ShowPage("modal") })
 
-	flex.AddItem(btnCopy, 0, 1, false)
-	flex.AddItem(tview.NewBox(), 1, 0, false)
-	flex.AddItem(btnMove, 0, 1, false)
-	flex.AddItem(tview.NewBox(), 1, 0, false)
 	flex.AddItem(btnRename, 0, 1, false)
 	flex.AddItem(tview.NewBox(), 1, 0, false)
 	flex.AddItem(btnQuit, 0, 1, false)
